29b11c12099378b4d70af8b24c359ac5545f0c0e
Match Chan <holymatch@gmail.com>
2018-09-03 18:46:35 +0800

    Add UpdateURL
    update url when the input field change

---
+++ Assets/Scripts/UpdateURL.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateURL : MonoBehaviour {
+
+    public InputField Field;
+    public void ValueChangeCheck()
+    {
+        WebRequestClient.baseurl = Field.text;
+    }
+}
