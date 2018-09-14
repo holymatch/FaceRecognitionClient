aa46f7cf58d9b5ae5c9b9524c8aa2d85df5ddc90
Match Chan <holymatch@gmail.com>
2018-09-14 17:41:44 +0800

    Remove Change Host Testbox and add WebService to change host IP

---
+++ Assets/UniWebServer/Scripts/ChangeFaceWebIP.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace UniWebServer
+{
+    [RequireComponent(typeof(EmbeddedWebServerComponent))]
+    public class ChangeFaceWebIP : MonoBehaviour, IWebResource
+    {
+
+        public string path = "/changeip";
+        EmbeddedWebServerComponent server;
+
+        public void HandleRequest(Request request, Response response)
+        {
+            Debug.Log("Request body: " + request.body);
+            var obj = JsonUtility.FromJson<ChangeIPObj>(request.body);
+            Debug.Log("Object: " + obj.host);
+            PlayerPrefs.SetString("host", obj.host);
+            response.statusCode = 200;
+            response.headers.AddHeaderLine("Content-Type:application/json");
+            response.message = "OK.";
+            response.Write("{\"Status\":\"OK\", \"host\":\"" + obj.host + "\"}");
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            server = GetComponent<EmbeddedWebServerComponent>();
+            server.AddResource(path, this);
+        }
+
+        [System.Serializable]
+        class ChangeIPObj
+        {
+            public string host;
+        }
+    }
+}
---
+++ Assets/UniWebServer/Scripts/CurrectFaceWebIP.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UniWebServer
+{
+    [RequireComponent(typeof(EmbeddedWebServerComponent))]
+    public class CurrectFaceWebIP : MonoBehaviour, IWebResource
+    {
+        public string path = "/checkip";
+        EmbeddedWebServerComponent server;
+
+        public void HandleRequest(Request request, Response response)
+        {
+            Debug.Log("Request body: " + request.body);
+            var host = PlayerPrefs.GetString("host");
+            response.statusCode = 200;
+            response.headers.AddHeaderLine("Content-Type:application/json");
+            response.message = "OK.";
+            response.Write("{\"Status\":\"OK\", \"host\":\"" + host + "\"}");
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            server = GetComponent<EmbeddedWebServerComponent>();
+            server.AddResource(path, this);
+        }
+    }
+}
